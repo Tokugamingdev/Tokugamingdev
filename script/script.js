dbb35dbb1054ed9c20cef678403999f69a362e7b
Tokugamingweb <febrianto.ladiku.3@gmail.com>
2022-04-05 10:15:20 +0800

    tokugamingdev

---
+++ script/script.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// pengelolaan angkot dengan pengulangan for:
+
+// var jmlAngkot = 10;
+// var angkotBeroperasi = 6;
+// var noAngkot = 1;
+
+// for (noAngkot = 1 ; noAngkot <= jmlAngkot ; noAngkot++  ) {
+//   if (noAngkot <= 6 && noAngkot !== 5 ) {
+//       console.log('Angkot No. ' + noAngkot + ' Beroperasi dengan baik.');
+//   } else if (noAngkot === 8 || noAngkot === 10 || noAngkot === 5) {
+//     console.log('Angkot No. ' + noAngkot + ' Sedang lembur');
+//   } else {
+//     console.log('Angkot No. ' + noAngkot + ' Tidak beroperasi');
+//   }
+// }
